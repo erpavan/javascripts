e5e7ebb7fec3b560a9d0d34625342590d2e871a2
Pavan <erpavan@gmail.com>
2023-12-02 12:33:09 +0530

    Update all modified files

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const p = require('path');
+
+
+let file = fs.readFileSync(p.join(__dirname, "data.json"))
+let content = file.toString()
+let obj = JSON.parse(content)
+console.log(obj.length)
+
+let firDiv = document.getElementById("first")
+console.log(firDiv)
